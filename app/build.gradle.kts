b948d4c566e060ee86ee6cbf677ca8f81925c270
mhshobuj <mhshobuj37@gmail.com>
2023-11-14 23:33:27 +0600

    gradle update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,29 +53,35 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
+    /** navigation component **/
     implementation ("androidx.navigation:navigation-fragment:2.7.4")
     implementation ("androidx.navigation:navigation-fragment-ktx:2.7.4")
     implementation ("androidx.navigation:navigation-ui-ktx:2.7.4")
-    //Coroutines
+
+    /** coroutines **/
     implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-    //Lifecycle
+
+    /** viewModel lifecycle **/
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
     implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
     implementation ("androidx.activity:activity-ktx:1.8.0")
-    //Dagger Hilt
+
+    /** dagger hilt **/
     implementation ("com.google.dagger:hilt-android:2.47")
     kapt ("com.google.dagger:hilt-compiler:2.47")
     kapt ("org.jetbrains.kotlinx:kotlinx-metadata-jvm:0.5.0")
-    //Gson
+
+    /** Gson converter **/
     implementation ("com.google.code.gson:gson:2.9.1")
-    //Retrofit
+
+    /** Retrofit & OkHttp **/
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
-    //OkHttp
     implementation ("com.squareup.okhttp3:okhttp:5.0.0-alpha.1")
     implementation ("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.1")
-    //Coil
+
+    /** Coil- image download from network **/
     implementation ("io.coil-kt:coil:2.2.2")
 }
